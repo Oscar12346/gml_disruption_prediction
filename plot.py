b3733eaf2d796b6ba76208f3111b7de3a4958881
maikdevries <dev.maik.devries@outlook.com>
2025-10-20 15:33:28 +0200

    feat: draw edges with disruptions as red

--- plot.py
+++ plot.py
@@ -19,7 +19,9 @@ def plot_snapshot(frame: int) -> None:
 	ax.clear()
 	ax.set_title(str(frame))
 
-	nx.draw(SNAPSHOTS[frame], **plot_options)
+	edge_colours = [ 'red' if w else 'black' for _, _, w in SNAPSHOTS[frame].edges.data('weight') ]
+
+	nx.draw(SNAPSHOTS[frame], edge_color = edge_colours, **plot_options)
 
 _ = FuncAnimation(fig, plot_snapshot, frames = len(SNAPSHOTS.keys()), interval = 250, repeat = True) # type: ignore
 
