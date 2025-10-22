d2dae2c2728a4c8b8bb60b3619ec7916842fcc6f
maikdevries <dev.maik.devries@outlook.com>
2025-10-22 17:19:52 +0200

    refactor: export single base graph with separate node lists by type

--- base_graph.py
+++ base_graph.py
@@ -5,17 +5,17 @@ from preprocess.train_stations import TRAIN_STATIONS
 from preprocess.weather_stations import WEATHER_STATIONS
 
 # [NOTE]
-graph = nx.Graph()
+BASE_GRAPH = nx.Graph()
 
-graph.add_nodes_from([ (code, row.to_dict() | { 'type': 'TRAIN' }) for code, row in TRAIN_STATIONS.iterrows() ])
-graph.add_nodes_from([ (code, row.to_dict() | { 'type': 'WEATHER' }) for code, row in WEATHER_STATIONS.iterrows() ])
+BASE_GRAPH.add_nodes_from([ (code, row.to_dict() | { 'type': 'TRAIN' }) for code, row in TRAIN_STATIONS.iterrows() ])
+BASE_GRAPH.add_nodes_from([ (code, row.to_dict() | { 'type': 'WEATHER' }) for code, row in WEATHER_STATIONS.iterrows() ])
 
 for _, row in CONNECTIONS.iterrows():
-	graph.add_edge(row['from'], row['to'], type = 'TRAIN')
+	BASE_GRAPH.add_edge(row['from'], row['to'], type = 'TRAIN')
 
 for code, row in TRAIN_STATIONS.iterrows():
-	graph.add_edge(code, row['weather_station'], type = 'WEATHER')
+	BASE_GRAPH.add_edge(code, row['weather_station'], type = 'WEATHER')
 
 
-BASE_GRAPH = graph.subgraph([ n for n, t in graph.nodes.data('type') if t == 'TRAIN' ])
-COMBINED_GRAPH = graph
+TRAIN_STATION_NODES = sorted([ n for n, t in BASE_GRAPH.nodes.data('type') if t == 'TRAIN' ])
+WEATHER_STATION_NODES = sorted([ n for n, t in BASE_GRAPH.nodes.data('type') if t == 'WEATHER' ])
--- preprocess/distances.py
+++ preprocess/distances.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import networkx as nx
 
-from base_graph import BASE_GRAPH
+from base_graph import BASE_GRAPH, TRAIN_STATION_NODES as nodes
 
 # [NOTE]
-nodes = sorted(BASE_GRAPH.nodes)
-df = pd.DataFrame(nx.floyd_warshall_numpy(BASE_GRAPH, nodelist = nodes), index = nodes, columns = nodes, dtype = int)
+df = pd.DataFrame(nx.floyd_warshall_numpy(BASE_GRAPH.subgraph(nodes), nodelist = nodes), index = nodes, columns = nodes, dtype = int)
 
 
 DISTANCES = df
