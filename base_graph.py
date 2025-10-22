b3b9254e3318b15fac8bf42d1d6aa30122f11637
maikdevries <dev.maik.devries@outlook.com>
2025-10-22 13:49:36 +0200

    feat: expose combined graph with weather stations and respective edges

--- base_graph.py
+++ base_graph.py
@@ -2,10 +2,20 @@ import networkx as nx
 
 from preprocess.connections import CONNECTIONS
 from preprocess.train_stations import TRAIN_STATIONS
+from preprocess.weather_stations import WEATHER_STATIONS
 
 # [NOTE]
-BASE_GRAPH = nx.Graph()
-BASE_GRAPH.add_nodes_from([ (code, row.to_dict()) for code, row in TRAIN_STATIONS.iterrows() ])
+graph = nx.Graph()
+
+graph.add_nodes_from([ (code, row.to_dict() | { 'type': 'TRAIN' }) for code, row in TRAIN_STATIONS.iterrows() ])
+graph.add_nodes_from([ (code, row.to_dict() | { 'type': 'WEATHER' }) for code, row in WEATHER_STATIONS.iterrows() ])
 
 for _, row in CONNECTIONS.iterrows():
-	BASE_GRAPH.add_edge(row['from'], row['to'])
+	graph.add_edge(row['from'], row['to'], type = 'TRAIN')
+
+for code, row in TRAIN_STATIONS.iterrows():
+	graph.add_edge(code, row['weather_station'], type = 'WEATHER')
+
+
+BASE_GRAPH = graph.subgraph([ n for n, t in graph.nodes.data('type') if t == 'TRAIN' ])
+COMBINED_GRAPH = graph
